84d77f6a05d739000d9855a076e89ea7837bdf31
Asyam_Jay <76540632+mAsyamJ@users.noreply.github.com>
2025-03-16 12:44:08 +0700

    Delete backend/testing.py

--- backend/testing.py
+++
@@ -1,6 +0,0 @@
-from voice_of_the_patient import record_audio
-
-# Test recording
-audio_filepath = "test_recording.mp3"
-record_audio(file_path=audio_filepath, timeout=5, phrase_time_limit=5)
-print(f"Recording saved to {audio_filepath}")
